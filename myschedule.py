6d69c00dd85385c03c538bd274503faef42bbd17
chang <mymail@qmail.com>
2018-10-09 11:27:14 +0800

    not use the schedule

--- myschedule.py
+++ myschedule.py
@@ -1,17 +1,21 @@
-import schedule
+#import schedule
 import time
 
 def job():
     from datetime import datetime
     import os
 
-    print("於{}開始抓取資料".format(datetime.now()))
+	a = datetime.now()
+	print("於{}開始抓取資料".format(a))
     load()
     print("於{}結束抓取資料\n".format(datetime.now()))
     os.system("git add .")
     os.system("git commit -m \"更新於{}\"".format(datetime.now()))
     os.system("git push -f origin master")
     os.system("git reset HEAD~1")
+	b = datetime.now()
+    print("於{}結束本次排程\n".format(b))
+	return (b - a).seconds
 
 def load():
     import requests
@@ -33,12 +37,12 @@ def load():
     fo.write(fstr)
     fo.close()
 
-schedule.every(30).minutes.do(job)
+#schedule.every(28).minutes.do(job)
 #schedule.every().day.at("07:12").do(job)
 #schedule.every().day.at("07:42").do(job)
 #schedule.every().day.at("08:12").do(job)
-job()
 
 while True:
-    schedule.run_pending()
-    time.sleep(1)
+    #schedule.run_pending()
+	delta = job()
+    time.sleep(1800 - delta)
