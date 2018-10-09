07af07af0891be80ac292b24267daa2a45e76874
chang <mymail@qmail.com>
2018-10-09 12:31:37 +0800

    fix tab

--- myschedule.py
+++ myschedule.py
@@ -5,17 +5,17 @@ def job():
     from datetime import datetime
     import os
 
-	a = datetime.now()
-	print("於{}開始抓取資料".format(a))
+    a = datetime.now()
+    print("於{}開始抓取資料".format(a))
     load()
     print("於{}結束抓取資料\n".format(datetime.now()))
     os.system("git add .")
     os.system("git commit -m \"更新於{}\"".format(datetime.now()))
     os.system("git push -f origin master")
     os.system("git reset HEAD~1")
-	b = datetime.now()
+    b = datetime.now()
     print("於{}結束本次排程\n".format(b))
-	return (b - a).seconds
+    return (b - a).seconds
 
 def load():
     import requests
@@ -44,5 +44,5 @@ def load():
 
 while True:
     #schedule.run_pending()
-	delta = job()
+    delta = job()
     time.sleep(1800 - delta)
